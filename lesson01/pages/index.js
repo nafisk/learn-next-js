73c877e21096673ee67ff5209266b6d698d08c12
nafisk <nafisrizwank@gmail.com>
2024-04-14 15:52:34 -0400

    add head tag

--- lesson01/pages/index.js
+++ lesson01/pages/index.js
@@ -2,12 +2,17 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import styles from '@/styles/Home.module.css';
+import Link from 'next/link';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      <Head>
+        <title>Next Complete Tutorial</title>
+      </Head>
+      <Link href='/about'>About</Link>
       <h1>Hello World</h1>
     </div>
   );
