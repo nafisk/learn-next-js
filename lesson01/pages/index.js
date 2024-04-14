d8c7697c32a39fb8f00c93b8a52a5be4f60fc2b6
nafisk <nafisrizwank@gmail.com>
2024-04-14 15:54:38 -0400

    add update css module

--- lesson01/pages/index.js
+++ lesson01/pages/index.js
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles.homePageTitle}>
       <Head>
         <title>Next Complete Tutorial</title>
       </Head>
